bd5a4ecc1e83cf7491f1269d399777efcc70a4ec
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2026-09-04 16:16:47 +0300

    Update application version to 1.0.2

--- src/main.py
+++ src/main.py
@@ -50,7 +50,7 @@ class ThrowdownApplication(Adw.Application):
         about = Adw.AboutDialog(application_name='Throwdown',
                                 application_icon='io.github.yioannides.Throwdown',
                                 developer_name='yiannis ioannides',
-                                version='1.0',
+                                version='1.0.2',
                                 issue_url='https://gitlab.com/yioannides/throwdown/issues/',
                                 translator_credits=_('translator-credits'),
                                 developers=['yiannis ioannides'],
